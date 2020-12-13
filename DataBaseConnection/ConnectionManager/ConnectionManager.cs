8533217d5ea347ac1a338ad543fc759c23b83393
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-12-13 22:13:00 +0330

    init Project

---
+++ DataBaseConnection/ConnectionManager/ConnectionManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseConnection.ConnectionManager
+{
+    public class ConnectionManager : IConnectionManager
+    {
+        public bool Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ConnectAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataBaseConnection/ConnectionManager/IConnectionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseConnection.ConnectionManager
+{
+    public interface IConnectionManager
+    {
+        Task<bool> ConnectAsync();
+        bool Connect();
+    }
+}
