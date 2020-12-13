c3f3014ac9d8828338f368a29a5b5339bc9969b6
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-12-13 23:36:09 +0330

    insert Services Manager

---
+++ Services/Manager/ICrudManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Services.Manager
+{
+    public interface ICrudManager
+    {
+        Task<bool> InsertAsync();
+        Task<bool> UpdateAsync();
+        Task<bool> DeleteAsync();
+        Task<bool> DeleteAsync(object Id);
+    }
+}
